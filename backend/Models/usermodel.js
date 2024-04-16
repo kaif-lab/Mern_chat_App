b9d922a5576aa0719f7ba0c8f0990ee24f4ce15e
Kaif <KaifAsShaikh@gmail.com>
2024-04-16 20:18:21 +0530

    Update usermodel.js

--- backend/Models/usermodel.js
+++ backend/Models/usermodel.js
@@ -1,21 +1,22 @@
-const  mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const user_schema = mongoose.Schema({
-
-    name : {type : String, required : true},
-    email : {type : String, required : true},
-    password : {type : String, required : true},
-    pic : {type : String, required : true,default:"https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
-},
-
-
-
-},
-{
-    timestamps : true
-}
+const user_schema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    pic: {
+      type: String,
+      required: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const  user = mongoose.model('user',user_schema);
+const user = mongoose.model("user", user_schema);
 
-module.exports = user
+module.exports = user;
