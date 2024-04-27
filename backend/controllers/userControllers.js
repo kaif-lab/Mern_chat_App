e0e5f82020644cb8b261f8ea9e36d08c66afaf01
Kaif <KaifAsShaikh@gmail.com>
2024-04-27 18:13:30 +0530

    update files

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -2,72 +2,79 @@ const asyncHandler = require("express-async-handler");
 
 const User = require("../Models/usermodel");
 const generateToken = require("../config/generateToken");
+const user = require("../Models/usermodel");
 
 const registerUser = asyncHandler(async (req, res) => {
   try {
-    
+    const { name, email, password, pic } = req.body;
 
-  const { name, email, password, pic } = req.body;
+    if (!name || !email || !password) {
+      res.status(400);
+      throw new Error("Please Enter all the Feilds");
+    }
 
-  if (!name || !email || !password) {
-    res.status(400);
-    throw new Error("Please Enter all the Feilds");
-  }
-
-  const userExists = await User.findOne({
-    email,
-  });
-  if (userExists) {
-    res.status(400);
-    throw new Error("User already exists");
-  }
-  const user = await User.create({
-    name,
-    email,
-    password,
-    pic,
-  });
-
-  if (user) {
-    res.status(201).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      pic: user.pic,
-      token:generateToken(user._id)
+    const userExists = await User.findOne({
+      email,
     });
-  }else{
-    res.status(400);
-    throw new Error("Something Went Wrong")
-  }
-} catch (error) {
-    
-}
+    if (userExists) {
+      res.status(400);
+      throw new Error("User already exists");
+    }
+    const user = await User.create({
+      name,
+      email,
+      password,
+      pic,
+    });
+
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        pic: user.pic,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error("Something Went Wrong");
+    }
+  } catch (error) {}
 });
 
-const authUser = (async(req,res) => { 
-
+const authUser = async (req, res) => {
   try {
-    
- 
-  const {email ,  password} = req.body;
+    const { email, password } = req.body;
 
-  const user = await User.findOne({email});
-  if(user && (await user.match_password(password))){
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      pic: user.pic,
-      token:generateToken(user._id)
-    })
-  }else{
-    res.status(401);
-    throw new Error("Invalid Email or Passowrd")
-  }
-} catch (error) {
-    
-}
+    const user = await User.findOne({ email });
+    if (user && (await user.match_password(password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        pic: user.pic,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401);
+      throw new Error("Invalid Email or Passowrd");
+    }
+  } catch (error) {}
+};
+
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $option: "i" } },
+          { email: { $regex: req.query.search, $option: "i" } },
+        ],
+      }
+    : {};
+
+  const users = await user.find(keyword)
+  // .find({ _id: { $ne: req.user._id } });
+res.send(users);
 });
 
-module.exports ={registerUser,authUser};
+module.exports = { registerUser, authUser, allUsers };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,15 @@
-const express  =  require('express');
-const { registerUser, authUser } = require('../controllers/userControllers');
+const express = require("express");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userControllers");
+// const { protect } = require("../middleware/authMiddleware");
 
-const router = express.Router()
+const router = express.Router();
 
-router.route('/').post(registerUser)
-router.post('/login',authUser)
+router.route("/").get(allUsers);
+router.route("/").post(registerUser);
+router.post("/login", authUser);
 
 module.exports = router;
